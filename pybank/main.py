feadb8621075c1e85d86a9a9f3b2aab7b052b156
Francis Marquez <francismarquez@Franciss-MacBook-Pro.local>
2021-02-27 19:54:53 -0500

    HW Update

--- pybank/main.py
+++ pybank/main.py
@@ -2,6 +2,16 @@
 import os
 import csv
 
+total_months = 0
+net_total = 0
+monthly_change = []
+monthly_count  = []
+greatest_increase = 0
+greatest_decrease = 0
+monthly_increase = 0
+monthly_decrease = 0
+
+
 
 #create file path
 budget_data_path = os.path.join("../pybank/resources/budget_data.csv")
@@ -9,4 +19,53 @@ budget_data_path = os.path.join("../pybank/resources/budget_data.csv")
 
 #open and read data path
 with open(budget_data_path) as budget_data_file:
-    reader = csv.reader(budget_data_file)
+    reader = csv.reader(budget_data_file , delimiiter=',')
+
+    #read the header row first
+    header = next(reader)
+    row = next(reader)
+
+    #determine how many total_months
+    previous_rows = int(row[1])
+    total_months = total_months + 1
+
+    #determine the total revenue
+    net_total = net_total + int(row[1])
+
+    #determine greatest increase
+    greatest_increase = int(row [1])
+    monthly_increase = row[0]
+
+    #read eaach row after headers
+    for row in reader:
+
+        #determine total number of months in dataset
+        total_months = total_months + 1
+
+        # determine net total profit/loss for whole period
+        net_total = net_total + int(row[1])
+
+        # determine the change for months
+        revenue_change = int(row [1] - previous_row)
+        monthly_change.append(revenue_change)
+        previous_row = int(row [1])
+        monthly_count.append(row[0])
+
+        #calculate the greatest monthly_decrease
+        if int(row[1] < greatest_increase:
+            greatest_increase = int(row[1])
+            monthly_decrease = row [0]
+    #determine the mean and the date
+    average_changes = sum(monthly_change) / len(monthly_change)
+
+    highest = max(monthly_change)
+    lowest = min(monthly_change)
+
+#print statemments
+print( "Financial Analysis")
+print("----------------------------")
+print("Total Months: {total_months}")
+print("Total Amount: ${net_total}")
+print("Greatest Incrase in Profits:,{monthly_increase}, (${highest})")
+print("Greatest Decrease in Profits:, {monthly_decrease},${lowest})")
+    
