6f9eff23c3bb8d683dcc9fa720811f499be45611
Francis Marquez <francismarquez@Franciss-MacBook-Pro.local>
2021-02-28 20:02:36 -0500

    Final HW push

--- pypoll/main1.py
+++ pypoll/main1.py
@@ -34,23 +34,22 @@ with open(voter_path) as voter_file:
             li_votes += 1
         else:
             otooley_votes += 1
-
-#determine percentage of votes per candidate
+    #determine percentage of votes per candidate
 percent_khan = khan_votes / total_votes
 percent_correy = correy_votes / total_votes
 percent_li = li_votes / total_votes
 percent_otooley = otooley_votes / total_votes
 
     #calculate the winner of the election via popular votes
-    popular_winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+popular_winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
 
-    if popular_winner == khan_votes:
+if popular_winner == khan_votes:
         name_of_winner = 'Khan'
-    elif popular_winner == correy_votes:
+elif popular_winner == correy_votes:
         name_of_winner = 'Correy'
-    elif popular_winner == li_votes:
+elif popular_winner == li_votes:
         name_of_winner = 'Li'
-    else:
+else:
         name_of_winner = 'O\'Tooley'
 
 #print election results
@@ -58,7 +57,7 @@ print(f'Election Results')
 print(f'--------------------------')
 print(f'Total Votes:{total_votes}')
 print(f'--------------------------')
-print(f'Kahn:{percent_khan:.3%} ({khan_votes})')
+print(f'Kahn:{percent_khan:.3%}({khan_votes})')
 print(f'Correy:{percent_correy:.3%} ({correy_votes}')
 print(f'Li:{percent_li:.3%} ({li_votes})')
 print(f'O\'Tooley:{percent_otooley:.3%}({correy_votes})')
@@ -67,21 +66,21 @@ print(f'The Winner Is:{name_of_winner}')
 print(f'--------------------------')
 
 #setting where to write edited data to
-results_voter_file = os.path.join('../pypoll/resources/election_data_results.txt')
+results_voter_file = os.path.join('../pypoll/analysis/election_data_results.txt')
 
 #open file as text voter_file
 
 with open(results_voter_file,'w',)as txtfile:
 
-#format election results
-txtfile.write(f'Election Results\n')
-txtfile.write(f'--------------------------\n')
-txtfile.write(f'Total Votes:{total_votes}\n')
-txtfile.write(f'--------------------------\n')
-txtfile.write(f'Kahn:{percent_khan:.3%} ({khan_votes})\n')
-txtfile.write(f'Correy:{percent_correy:.3%}({correy_votes}\n')
-txtfile.write(f'Li:{percent_li:.3%} ({li_votes})\n')
-txtfile.write(f'O\'Tooley:{percent_otooley:.3%}({correy_votes})\n')
-txtfile.write(f'--------------------------\n')
-txtfile.write(f'The Winner Is:{name_of_winner}\n')
-txtfile.write(f'--------------------------\n')
+    #format election results
+    txtfile.write(f'Election Results\n')
+    txtfile.write(f'--------------------------\n')
+    txtfile.write(f'Total Votes:{total_votes}\n')
+    txtfile.write(f'--------------------------\n')
+    txtfile.write(f'Kahn:{percent_khan:.3%} ({khan_votes})\n')
+    txtfile.write(f'Correy:{percent_correy:.3%}({correy_votes}\n')
+    txtfile.write(f'Li:{percent_li:.3%} ({li_votes})\n')
+    txtfile.write(f'O\'Tooley:{percent_otooley:.3%}({correy_votes})\n')
+    txtfile.write(f'--------------------------\n')
+    txtfile.write(f'The Winner Is:{name_of_winner}\n')
+    txtfile.write(f'--------------------------\n')
